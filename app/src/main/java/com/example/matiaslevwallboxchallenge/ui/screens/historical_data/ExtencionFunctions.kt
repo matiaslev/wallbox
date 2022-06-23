7b5ffa0b6380b842e63a86ff639b507ff6ff6e3f
matiaslev <matias.lev@astropay.com>
2022-06-23 02:05:26 +0200

    historical data chart dates

--- app/src/main/java/com/example/matiaslevwallboxchallenge/ui/screens/historical_data/ExtencionFunctions.kt
+++ app/src/main/java/com/example/matiaslevwallboxchallenge/ui/screens/historical_data/ExtencionFunctions.kt
@@ -2,6 +2,7 @@ package com.example.matiaslevwallboxchallenge.ui.screens.historical_data
 
 import com.example.domain.models.HistoricalDataItem
 import com.github.mikephil.charting.data.Entry
+import java.time.ZoneOffset
 import java.time.ZonedDateTime
 
 fun HistoricalDataItem.toSolarPanelPowerEntry() = Entry(
@@ -12,4 +13,16 @@ fun HistoricalDataItem.toSolarPanelPowerEntry() = Entry(
 fun HistoricalDataItem.toGridPowerEntry() = Entry(
     ZonedDateTime.parse(timestamp).toEpochSecond().toFloat(),
     gridActivePower.toFloat()
+)
+
+fun HistoricalDataItem.toQuasarsActivePowerEntry() = Entry(
+    ZonedDateTime.parse(timestamp).toEpochSecond().toFloat(),
+    /**
+     * If the value is negative it means it is discharging to the building
+     * to supply the building demand
+     * **
+     * we are only showing sources that supplies the building energy demand as on
+     * the widget that navigates to this screen
+     */
+    if (quasarsActivePower < 0) quasarsActivePower.toFloat() * -1 else 0f
 )
--- app/src/main/java/com/example/matiaslevwallboxchallenge/ui/screens/historical_data/HistoricalDataScreen.kt
+++ app/src/main/java/com/example/matiaslevwallboxchallenge/ui/screens/historical_data/HistoricalDataScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.domain.models.HistoricalDataItem
+import com.example.matiaslevwallboxchallenge.R
 import com.example.matiaslevwallboxchallenge.ui.theme.MatiasLevWallboxChallengeTheme
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.XAxis
@@ -19,7 +20,10 @@ import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.ValueFormatter
 import org.koin.androidx.compose.get
-import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 @Composable
@@ -56,7 +60,7 @@ fun LineChartView(
                     // Solar Panel
                     LineDataSet(
                         historicalData.map { it.toSolarPanelPowerEntry() },
-                        "solar" // not used
+                        context.getString(R.string.solar_panels)
                     ).apply {
                         setDrawCircles(false)
                         lineWidth = 3f
@@ -66,22 +70,36 @@ fun LineChartView(
                     // Grid
                     LineDataSet(
                         historicalData.map { it.toGridPowerEntry() },
-                        "grid" // not used
+                        context.getString(R.string.from_the_grid)
                     ).apply {
                         setDrawCircles(false)
                         lineWidth = 3f
                         setDrawFilled(false)
                         color = Color.GREEN
-                    }
+                    },
+                    // Quasars
+                    LineDataSet(
+                        historicalData.map { it.toQuasarsActivePowerEntry() },
+                        context.getString(R.string.quasars_supplying_building)
+                    ).apply {
+                        setDrawCircles(false)
+                        lineWidth = 3f
+                        setDrawFilled(false)
+                        color = Color.BLUE
+                    },
                 )
 
                 xAxis.apply {
                     setDrawGridLines(false)
                     position = XAxis.XAxisPosition.BOTTOM
-                    setLabelCount(8, true)
                     valueFormatter = object : ValueFormatter() {
                         override fun getFormattedValue(value: Float): String {
-                            val date = Date(value.toLong() * 1000)
+                            //val date = Instant.ofEpochMilli(value.toLong() * 1000).atZone(ZoneOffset.of("+00:00")).toLocalDate();
+                            // val date = Date(value.toLong() * 1000)
+                            val date: LocalDateTime = LocalDateTime.ofInstant(
+                                Instant.ofEpochMilli(value.toLong() * 1000),
+                                ZoneOffset.of("+00:00")
+                            )
                             return getChartDateFormatHourOfDay(date)
                         }
                     }
@@ -100,10 +118,9 @@ fun LineChartView(
     )
 }
 
-fun getChartDateFormatHourOfDay(date: Date): String {
-    val chartFormatHourOfDay = "hh a"
-    val format = SimpleDateFormat(chartFormatHourOfDay, Locale.getDefault())
-    return format.format(date)
+fun getChartDateFormatHourOfDay(date: LocalDateTime): String {
+    val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd hh a")
+    return date.format(dateTimeFormatter)
 }
 
 @Composable
