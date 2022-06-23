08096efd115ba0b925e803641ec0b67afd7ccc2f
matiaslev <matias.lev@astropay.com>
2022-06-23 17:43:46 +0200

    GetLiveDataTest.kt

---
+++ domain/src/test/java/com/astropaycard/domain/actions/GetLiveDataTest.kt
@@ -0,0 +1,79 @@
+package com.astropaycard.domain.actions
+
+import com.astropaycard.domain.base.shouldBeEqualTo
+import com.example.domain.actions.GetLiveData
+import com.example.domain.base.ResultWrapper
+import com.example.domain.models.LiveData
+import com.example.domain.models.QuasarAction
+import com.example.domain.repositories.ApiRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(JUnit4::class)
+class GetLiveDataTest {
+    private lateinit var getLiveData: GetLiveData
+
+    @MockK
+    internal lateinit var apiRepository: ApiRepository
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        getLiveData = GetLiveData(apiRepository)
+    }
+
+    @Test
+    fun `should return ResultSuccess when GetLiveData returns Success`() = runTest {
+        // given
+        val liveData = LiveData(
+            solarPower = 7.827,
+            absoluteQuasarsPower = 38.732,
+            quasarAction = QuasarAction.SupplyingBuilding,
+            gridPower = 80.475,
+            buildingDemand = 127.03399999999999,
+            systemSoc = 48.333333333333336,
+            totalEnergy = 960,
+            currentEnergy = 464.0
+        )
+        coEvery { apiRepository.getLiveData() } returns ResultWrapper.Success(liveData)
+
+        // when
+        val result = getLiveData()
+
+        // then
+        result shouldBeEqualTo GetLiveData.Result.Success(liveData = liveData)
+    }
+
+    @Test
+    fun `should return ResultError when GetLiveData returns Error`() = runTest {
+        // given
+        val message = "please contact us!"
+        coEvery { apiRepository.getLiveData() } returns ResultWrapper.Error(message)
+
+        // when
+        val result = getLiveData()
+
+        // then
+        result shouldBeEqualTo GetLiveData.Result.ErrorResponse(message)
+    }
+
+    @Test
+    fun `should return ResultNetworkError when GetLiveData returns NetworkError`() = runTest {
+        // given
+        coEvery { apiRepository.getLiveData() } returns ResultWrapper.NetworkError
+
+        // when
+        val result = getLiveData()
+
+        // then
+        result shouldBeEqualTo GetLiveData.Result.NetworkError
+    }
+}
