6d8083ef9738917115810920daf5c50d9ab62cb1
matiaslev <matias.lev@astropay.com>
2022-06-26 17:07:21 +0200

    ExtensionFunctionsTest.kt

--- app/src/test/java/com/example/matiaslevwallboxchallenge/extension_functions/ExtensionFunctionsTest.kt
+++ app/src/test/java/com/example/matiaslevwallboxchallenge/extension_functions/ExtensionFunctionsTest.kt
@@ -9,8 +9,6 @@ import org.junit.Test
 
 class ExtensionFunctionsTest {
 
-
-
     @Test
     fun getPieChartPowerValues_not_shouldAddValues_whenAreZero() {
         val liveData = MockDomainData.liveDataMock(
